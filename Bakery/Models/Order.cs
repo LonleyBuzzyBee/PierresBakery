a308d7735b71ca377cab967eb2c4a948e30b3fe8
mai cook <mailiecook@yahoo.com>
2020-05-22 13:21:50 -0700

    built out order.cs

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -4,15 +4,31 @@ namespace Bakery.Models
 {
   public class Order
   {
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { };
     public string Description { get; set; }
     public double Price { get; set; }
     public string Date { get; set; }
 
     public Order(string description, double price, string date)
     {
+
       Description = description;
+      _instances.Add(this);
       Price = price;
+      _instances.Add(this);
       Date = date;
+      _instances.Add(this);
+      Id = _instances.Count;
+
+    }
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId - 1];
     }
 
   }
--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -27,6 +27,9 @@ namespace Bakery.Models
     {
       return _instances[searchId - 1];
     }
-
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
   }
 }
