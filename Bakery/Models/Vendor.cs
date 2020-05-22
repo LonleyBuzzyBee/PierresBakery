7f341330fdd572f17454ece326514881c37273a2
mai cook <mailiecook@yahoo.com>
2020-05-22 11:46:26 -0700

    add constructors for vener and order

--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -4,6 +4,16 @@ namespace Bakery.Models
 {
   public class Vendor
   {
-    
+    private static List<Vendor> _instances = new List<Vendor> { };
+    public int Id { get; }
+    public string Name { get; set; }
+    public List<Order> Orders { get; set; }
+    public Vendor(string name)
+    {
+      Name = name;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order> { };
+    }
   }
 }
